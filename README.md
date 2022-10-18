9721c471c840e62bcb51886b37c10decdad80d87
신지연/(협력사) 사원/한국영업업무혁신팀(jy0731.shin@lgepartner.com) <jeeyeon654@gmail.com>
2022-10-19 07:55:43 +0900

    리액트 기초(바닐라js 구현 후 리액트로 구현해보기)

--- README.md
+++ README.md
@@ -16,3 +16,18 @@ npm install webpack webpack-cli react react-dom
 ```bash
 npx webpack
 ```
+
+# React
+
+## 실행
+
+```bash
+npm start
+set HTTPS=true && npm start
+```
+
+## 빌드
+
+```bash
+npx serve -s build
+```
---
+++ css2-module/src/Button.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Style from "./Button.module.css";
+import cn from "classnames";
+
+export default function Button({ size }) {
+  const isBig = size === "big";
+  return (
+    <button
+      className={cn(Style.button, {
+        [Style.big]: isBig,
+        [Style.small]: !isBig,
+      })}
+    >
+      {isBig ? "큰 버튼" : "작은 버튼"}
+    </button>
+  );
+  // if(size === 'big'){
+  //   return <button className={cn(Style.button, Style.big)}>큰 버튼</button>
+  // }else{
+  //   return(
+  //     <button className={`${Style.button} ${Style.small}`}>작은 버튼</button>
+  //   )
+  // }
+}
